8b97c9151e605e45c7ec5814216881314eeb5716
Michael <mclendon95@yahoo.com>
2022-11-06 14:06:21 -0800

    update

--- Project_3/templates/app.py
+++ Project_3/templates/app.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/Project3/Project_3/templates")
 def index():
-    return render_template('index.html')
+    return render_template('main.index.html')
 
-@app.route('/co2_filtered.csv')
-def data():
-    return jsonify({'results'})
 
 
 if __name__ == '__main__':
